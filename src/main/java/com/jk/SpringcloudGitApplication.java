05959f4316e2429e00bcb03cba2ae6a4baff8746
jiangtaoyun <535103200@qq.com>
2019-07-03 17:11:17 +0800

    注é提交代码

---
+++ src/main/java/com/jk/SpringcloudGitApplication.java
@@ -0,0 +1,13 @@
+package com.jk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudGitApplication.class, args);
+    }
+
+}
